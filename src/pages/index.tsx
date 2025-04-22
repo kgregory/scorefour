7f8496dfda4f747bb5d28febe6235c7cd82cb7b7
Ken Gregory <kgreg80@gmail.com>
2025-04-22 17:08:24 -0400

    Update so all winning directions (and stretches longer than 4) are marked for the winner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,231 @@ import * as React from "react";
 const rows = 6;
 const columns = 7;
 
+interface Board<TValue = number> {
+  values: Array<TValue>;
+  columns: number;
+  rows: number;
+}
+
+type Direction = "n" | "ne" | "e" | "se" | "s" | "sw" | "w" | "nw";
+
+const adjacentCellIndexes = new Map();
+
+/** get the indexes of the adjacent cells in each direction, where applicable and memoize using a map keyed by the cell */
+const getAdjacentCellIndexes = <TValue,>(
+  cell: number,
+  board: Board<TValue>,
+) => {
+  const { columns, rows } = board;
+
+  // key considers board-size
+  const cellKey = `${cell}_${rows}_${columns}`;
+
+  if (adjacentCellIndexes.has(cellKey)) {
+    return adjacentCellIndexes.get(cellKey);
+  }
+
+  // there might not be an adjacent cell in all directions
+  const row = Math.floor(cell / columns);
+  const hasN = cell >= columns;
+  const hasE = Math.floor((cell + 1) / columns) <= row;
+  const hasS = cell <= (rows - 1) * columns - 1;
+  const hasW = Math.floor((cell - 1) / columns) >= row;
+  const hasNW = hasN && hasW;
+  const hasNE = hasN && hasE;
+  const hasSW = hasS && hasW;
+  const hasSE = hasS && hasE;
+
+  // relative indices for each direction
+  const n = cell - columns;
+  const w = cell - 1;
+  const e = cell + 1;
+  const s = cell + columns;
+  const ne = n + 1;
+  const nw = n - 1;
+  const se = s + 1;
+  const sw = s - 1;
+
+  adjacentCellIndexes.set(cellKey, {
+    n: hasN ? n : undefined,
+    w: hasW ? w : undefined,
+    e: hasE ? e : undefined,
+    s: hasS ? s : undefined,
+    ne: hasNE ? ne : undefined,
+    nw: hasNW ? nw : undefined,
+    se: hasSE ? se : undefined,
+    sw: hasSW ? sw : undefined,
+  });
+
+  return adjacentCellIndexes.get(cellKey);
+};
+
+/** get cell index for each adjacent position, by direction */
+const getAdjacentCells = <TValue,>(cell: number, board: Board<TValue>) => {
+  const { values } = board;
+
+  const { n, w, e, s, ne, se, sw, nw } = getAdjacentCellIndexes(cell, board);
+
+  // return adjacent cells with matching values
+  const value = values[cell];
+  return {
+    n: n != null && values[n] === value ? n : undefined,
+    ne: ne != null && values[ne] === value ? ne : undefined,
+    e: e != null && values[e] === value ? e : undefined,
+    se: se != null && values[se] === value ? se : undefined,
+    s: s != null && values[s] === value ? s : undefined,
+    sw: sw != null && values[sw] === value ? sw : undefined,
+    w: w != null && values[w] === value ? w : undefined,
+    nw: nw != null && values[nw] === value ? nw : undefined,
+  };
+};
+
+/** starting from the specified cell index, continuing in the specified direction, return an array of indexes for continuously matching cells */
+const checkDirection = <TValue,>({
+  cell,
+  value,
+  direction,
+  board,
+}: {
+  cell: number | undefined;
+  value: TValue;
+  direction: Direction;
+  board: Board<TValue>;
+}): number[] => {
+  const { values } = board;
+
+  if (cell == null || values[cell] !== value) {
+    return [];
+  }
+
+  const adjacentCell = getAdjacentCells(cell, board)[direction];
+
+  return [
+    cell,
+    ...checkDirection({ cell: adjacentCell, value, direction, board }),
+  ];
+};
+
+/** check horizontal, vertical, and diagonal from the specified cell index */
+const checkCell = <TValue,>(cell: number, board: Board<TValue>) => {
+  const { values } = board;
+
+  const value = values[cell];
+
+  if (value == null) {
+    return {
+      vertical: [],
+      horizontal: [],
+      diagonal: [],
+      reverseDiagonal: [],
+    };
+  }
+
+  const { n, ne, e, se, s, sw, w, nw } = getAdjacentCells(cell, board);
+
+  const vertical = [
+    cell,
+    ...checkDirection({ cell: n, direction: "n", value, board }),
+    ...checkDirection({ cell: s, direction: "s", value, board }),
+  ];
+
+  const horizontal = [
+    cell,
+    ...checkDirection({ cell: w, direction: "w", value, board }),
+    ...checkDirection({ cell: e, direction: "e", value, board }),
+  ];
+
+  const diagonal = [
+    cell,
+    ...checkDirection({ cell: nw, direction: "nw", value, board }),
+    ...checkDirection({ cell: se, direction: "se", value, board }),
+  ];
+
+  const reverseDiagonal = [
+    cell,
+    ...checkDirection({ cell: ne, direction: "ne", value, board }),
+    ...checkDirection({ cell: sw, direction: "sw", value, board }),
+  ];
+
+  return {
+    vertical,
+    horizontal,
+    diagonal,
+    reverseDiagonal,
+  };
+};
+
+const useGameState = () => {
+  const [board, setBoard] = React.useState<
+    Array<"red" | "yellow" | "win" | undefined>
+  >(Array(rows * columns).fill(undefined));
+  const [player, setPlayer] = React.useState<"red" | "yellow">("red");
+  const [winner, setWinner] = React.useState<"red" | "yellow" | "draw">();
+
+  return React.useMemo(
+    () => ({
+      board,
+      player,
+      winner,
+      reset: () => {
+        setBoard(Array(rows * columns).fill(undefined));
+        setPlayer("red");
+        setWinner(undefined);
+      },
+      update: (column: number) => {
+        if (winner != null) {
+          // nothing to do when the game's over (someone won or the players tied)
+          return;
+        }
+
+        const newBoard = [...board];
+
+        // check the column from the bottom up
+        let cell = undefined;
+        for (let i = rows - 1; i >= 0; i -= 1) {
+          if (newBoard[i * columns + column] == null) {
+            newBoard[i * columns + column] = player;
+            cell = i * columns + column;
+            break;
+          }
+        }
+
+        if (cell == null) {
+          // no need to check the board for an invalid move
+          return;
+        }
+
+        // filter the adjoining cells by the directions that have at least 4 in a row
+        const winningCells = Object.values(
+          checkCell(cell, { columns, rows, values: newBoard }),
+        )
+          .filter((direction) => direction.length > 3)
+          .flat();
+
+        // mark winning cells, if any
+        winningCells.forEach((i) => {
+          newBoard[i] = "win";
+        });
+
+        setWinner((winner) => {
+          if (winningCells.length > 0) {
+            return player;
+          }
+          if (newBoard.every((item) => item != null)) {
+            // no winner and we're out of moves
+            return "draw";
+          }
+          return winner;
+        });
+
+        setBoard(newBoard);
+        setPlayer(player === "red" ? "yellow" : "red");
+      },
+    }),
+    [board, player, winner],
+  );
+};
+
 interface CircleProps {
   onClick: () => void;
   variant?: "empty" | "red" | "yellow" | "win";
@@ -65,114 +290,7 @@ const Board = (props: BoardProps) => {
 };
 
 const Game = () => {
-  const [board, setBoard] = React.useState<
-    Array<"red" | "yellow" | "win" | undefined>
-  >(Array(rows * columns).fill(undefined));
-  const [player, setPlayer] = React.useState<"red" | "yellow">("red");
-  const [winner, setWinner] = React.useState<"red" | "yellow" | "draw">();
-
-  const handleTurn = (column: number) => {
-    if (winner != null) {
-      return;
-    }
-
-    const newBoard = [...board];
-    // check the column from the bottom up
-    let validMove = false;
-    for (let i = rows - 1; i >= 0; i -= 1) {
-      if (newBoard[i * columns + column] == null) {
-        newBoard[i * columns + column] = player;
-        validMove = true;
-        break;
-      }
-    }
-
-    let isWinningMove = false;
-
-    // row check
-    for (let i = 0; i < rows; i++) {
-      for (let j = 0; j < columns - 3; j++) {
-        if (
-          newBoard[i * columns + j] === player &&
-          newBoard[i * columns + j + 1] === player &&
-          newBoard[i * columns + j + 2] === player &&
-          newBoard[i * columns + j + 3] === player
-        ) {
-          newBoard[i * columns + j] = "win";
-          newBoard[i * columns + j + 1] = "win";
-          newBoard[i * columns + j + 2] = "win";
-          newBoard[i * columns + j + 3] = "win";
-          isWinningMove = true;
-        }
-      }
-    }
-
-    // column check
-    for (let i = 0; i < rows - 3; i++) {
-      for (let j = 0; j < columns; j++) {
-        if (
-          newBoard[i * columns + j] === player &&
-          newBoard[(i + 1) * columns + j] === player &&
-          newBoard[(i + 2) * columns + j] === player &&
-          newBoard[(i + 3) * columns + j] === player
-        ) {
-          newBoard[i * columns + j] = "win";
-          newBoard[(i + 1) * columns + j] = "win";
-          newBoard[(i + 2) * columns + j] = "win";
-          newBoard[(i + 3) * columns + j] = "win";
-          isWinningMove = true;
-        }
-      }
-
-      // diagonal check
-      for (let j = 0; j < columns - 3; j++) {
-        if (
-          newBoard[i * columns + j] === player &&
-          newBoard[(i + 1) * columns + j + 1] === player &&
-          newBoard[(i + 2) * columns + j + 2] === player &&
-          newBoard[(i + 3) * columns + j + 3] === player
-        ) {
-          newBoard[i * columns + j] = "win";
-          newBoard[(i + 1) * columns + j + 1] = "win";
-          newBoard[(i + 2) * columns + j + 2] = "win";
-          newBoard[(i + 3) * columns + j + 3] = "win";
-          isWinningMove = true;
-        }
-        if (
-          newBoard[i * columns + j + 3] === player &&
-          newBoard[(i + 1) * columns + j + 2] === player &&
-          newBoard[(i + 2) * columns + j + 1] === player &&
-          newBoard[(i + 3) * columns + j] === player
-        ) {
-          newBoard[i * columns + j + 3] = "win";
-          newBoard[(i + 1) * columns + j + 2] = "win";
-          newBoard[(i + 2) * columns + j + 1] = "win";
-          newBoard[(i + 3) * columns + j] = "win";
-          isWinningMove = true;
-        }
-      }
-    }
-
-    if (validMove) {
-      setWinner((winner) => {
-        if (isWinningMove) {
-          return player;
-        }
-        if (newBoard.every((item) => item != null)) {
-          return "draw";
-        }
-        return winner;
-      });
-      setBoard(newBoard);
-      setPlayer(player === "red" ? "yellow" : "red");
-    }
-  };
-
-  const handleReset = () => {
-    setBoard(Array(rows * columns).fill(undefined));
-    setPlayer("red");
-    setWinner(undefined);
-  };
+  const { board, player, winner, update, reset } = useGameState();
 
   return (
     <div className="flex flex-col items-center gap-8">
@@ -185,10 +303,10 @@ const Game = () => {
       ) : (
         <div>It&apos;s {player}&apos;s turn</div>
       )}
-      <button onClick={handleReset} className="btn">
+      <button onClick={reset} className="btn">
         Reset
       </button>
-      <Board data={board} handleTurn={handleTurn} />
+      <Board data={board} handleTurn={update} />
     </div>
   );
 };
